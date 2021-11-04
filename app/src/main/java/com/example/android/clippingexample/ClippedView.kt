c6281401231811c525d91803a77862af0348fec0
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-11-04 15:57:51 -0700

    create and initialize paint and path

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -28,4 +28,32 @@ class ClippedView @JvmOverloads constructor(
 
 //    create and initialize path variable of Path to store locally path of whats been drawn
     private val path = Path()
+
+//    variables for dimensions for clipping rectangle around the whole set of shapes
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+//    variables for the inset of a rectangle and offset of a small rectangle
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimensionPixelOffset(R.dimen.smallRectOffset)
+
+//    variable for radius of circle. This circle is drawn inside rectangle
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+//    offset and text size for text that is drawn inside rectangle
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+//    set up coordinates for two columns
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+//    add coordinate for each row, including the final row for transformed text
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
 }
