41fdc09a6929ff85159e092c85c320043821410c
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-11-04 14:04:36 -0700

    file created

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
+//custom view extends View. '@JvmOverloads' annotation instructs Kotlin compiler to generate
+//overloads for this function that substitute default parameter values
 class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
