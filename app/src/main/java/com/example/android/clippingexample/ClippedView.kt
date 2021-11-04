37a5428129db44dfcc51b67874e44f14b9683db2
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-11-04 15:16:01 -0700

    create and initialize paint and path

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.android.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,4 +12,20 @@ class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {}
+) : View(context, attrs, defStyleAttr) {
+
+//    create and initialize Paint
+    private val paint = Paint().apply {
+
+//        smooth out edjes of what is drawn without affecting shape
+//        enable anti-aliasing
+        isAntiAlias = true
+//        use stroke width
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+//        define text-size
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+//    create and initialize path variable of Path to store locally path of whats been drawn
+    private val path = Path()
+}
